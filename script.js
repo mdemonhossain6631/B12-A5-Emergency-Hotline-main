cedd82dd3d35598d3690bd08999d5a8809529575
Md. Emon Hossain <mdemonhossain20158@gmail.com>
2025-08-29 00:53:50 +0600

    My 2nd Commit

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('text script')
+
+// increase heart icon
+const loveCount = document.getElementsByClassName("love-count")[0]
+const loveIcon = document.getElementsByClassName('love-icon')
+
+for(const icon of loveIcon){
+    icon.addEventListener("click", function(){
+
+        const loveValue = parseInt(loveCount.innerText);
+
+        loveCount.innerText = loveValue + 1;
+    })
+}
